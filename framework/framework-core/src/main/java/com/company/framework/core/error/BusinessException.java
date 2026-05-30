d476239deda93eac0ccc33936889575010df6b64
프루시안블루 <frusianblue@gmail.com>
2026-05-30 13:11:39 +0900

    에러 수정

--- framework/framework-core/src/main/java/com/company/framework/core/error/BusinessException.java
+++ framework/framework-core/src/main/java/com/company/framework/core/error/BusinessException.java
@@ -4,6 +4,7 @@ package com.company.framework.core.error;
  * 업무 로직에서 의도적으로 던지는 예외. GlobalExceptionHandler가 ErrorCode를 그대로 응답으로 변환한다.
  */
 public class BusinessException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
     private final transient ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
